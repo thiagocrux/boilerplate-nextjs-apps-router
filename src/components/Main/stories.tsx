7de97f060c7d50a27b11f86424ae623e2b1108ac
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-10-07 15:15:46 -0300

    finish storybook config

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -4,7 +4,22 @@ import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  argTypes: {
+    title: {
+      control: { type: 'text' }
+    },
+    description: {
+      control: { type: 'text' }
+    }
+  }
 } as Meta
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'basic title',
+    description: 'basic description'
+  }
+}
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -21,6 +21,6 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     // verifica se o background-color está correto
-    expect(container.firstChild).toHaveStyle({ 'background-color': '#ffffff' })
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
